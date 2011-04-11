803f47e45e375418476a4c9a051d63ed4453dcfa
Shane <dickson.shane@gmail.com>
2011-04-11 20:31:35 +1000

    updated README, adding button as link styling, ordering selections by votes

--- app/models/selection.rb
+++ app/models/selection.rb
@@ -2,6 +2,8 @@ class Selection < ActiveRecord::Base
     belongs_to :user
     belongs_to :event
     
+    default_scope :order => 'selections.votes DESC'
+    
     validates :event_id, :presence => true
     validates :user_id, :presence => true
     validates :movie_id, :presence => true
