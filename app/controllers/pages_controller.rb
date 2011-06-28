0e71ff691fa3cdc5150708e1305a4b09bb1aca64
Shane <dickson.shane@gmail.com>
2011-06-28 22:25:51 +1000

    added recent films to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
+  Tmdb.api_key = "9027009be089788945e1c7aa516338a2"
+  
   def home
       @title = "Home"
+      @recent_movies = TmdbMovie.browse(:order_by => "release",:order => "desc", :release_min => 1.year.ago.to_i,:release_max => Time.now.to_i, :page => 1, :per_page => 10, :language => "en", :expand_results => false)
   end
 
   def contact
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,9 @@
 module PagesHelper
+  def insert_poster(movie,height)
+    if movie.posters.nil? or movie.posters.first.nil?
+      image_tag 'no_poster.jpg', :height => height 
+    else
+      image_tag movie.posters.first.url, :alt => "no image", :height => height
+    end
+  end
 end
