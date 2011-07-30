549aea89b9ad38f5443ee1758c522fc39c8f180b
Shane Dickson <dickson.shane@gmail.com>
2011-07-30 11:24:02 +1000

    change movie index to only fetch thumbnails, should make it a bit quicker

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -7,6 +7,20 @@ module MoviesHelper
     end
   end
   
+  def insert_thumbnail(movie,height)
+    if movie.posters.nil? or movie.posters.first.nil?
+      image_tag 'no_poster.jpg', :height => height 
+    else
+      movie.posters.each do |p|
+        if p.size == "thumb"
+          return image_tag p.url, :alt => "no image", :height => height
+        end
+      end
+    end
+    #catch all in case we don't find a thumbnail
+    image_tag 'no_poster.jpg', :height => height 
+  end
+  
   def get_poster_url(movie)
     if movie.posters.nil? or movie.posters.first.nil?
       'no_poster.jpg'
