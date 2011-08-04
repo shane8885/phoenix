239dbde6211f07cbf1572a1afb4c10f754dc73a2
Shane Dickson <dickson.shane@gmail.com>
2011-08-04 17:46:52 +1000

    changed home page to display thumbnails for movies

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,9 +1,15 @@
-module PagesHelper
-  def insert_poster(movie,height)
+module PagesHelper  
+  def insert_thumbnail(movie,height)
     if movie.posters.nil? or movie.posters.first.nil?
       image_tag 'no_poster.jpg', :height => height 
     else
-      image_tag movie.posters.first.url, :alt => "no image", :height => height
+      movie.posters.each do |p|
+        if p.size == "thumb"
+          return image_tag p.url, :alt => "no image", :height => height
+        end
+      end
     end
+    #catch all in case we don't find a thumbnail
+    image_tag 'no_poster.jpg', :height => height 
   end
 end
