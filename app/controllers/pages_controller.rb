432cd274729512d4bb6f9f5fa7f1b2b9742c7872
Shane Dickson <shane@gecko.(none)>
2012-01-09 20:38:10 +1100

    added suggestion box to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,8 @@ class PagesController < ApplicationController
   
   def home
       @title = "Home"
-      @recent_movies = TmdbMovie.browse(:order_by => "release",:order => "desc", :release_min => 1.year.ago.to_i,:release_max => Time.now.to_i, :page => 1, :per_page => 10, :language => "en", :certifications => "G,PG,PG-13,R", :expand_results => false)
+      @recent_movies = TmdbMovie.browse(:order_by => "release",:order => "desc", :release_min => 1.year.ago.to_i,:release_max => Time.now.to_i, :page => 1, :per_page => 10, :language => "en", :certifications => "G,PG,PG-13,R", :countries => "au,us,gb", :expand_results => false)
+      @suggested_movie = TmdbMovie.browse(:order_by => "rating", :order => "desc", :min_votes => 3, :page => rand(30), :per_page => 10, :language => "en", :certifications => "G,PG,PG-13,R", :expand_results => false)[rand(10)]
   end
 
   def contact
